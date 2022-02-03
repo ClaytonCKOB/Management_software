55be609cb94c397eab2651733541f288ae80c85a
ClaytonCKOB <ti@reflexapersianas.com.br>
2022-02-03 17:29:34 -0300

    Creating the method insertProduct

--- src/classes/connDatabase.py
+++ src/classes/connDatabase.py
@@ -6,6 +6,10 @@ class connDatabase:
         self.conn = pyodbc.connect(r'DRIVER={Microsoft Access Driver (*.mdb, *.accdb)};DBQ=C:\Users\clayt\OneDrive\Documentos\Management_software\database\login.accdb;')
         self.cursor = self.conn.cursor()
 
+    def insertProduct(self, pedido, cliente, modelo, colecao, cor, acionamento, largura, altura, tubo):
+        self.cursor.execute('INSERT INTO pedido (pedido, cliente, modelo, colecao, cor, acionamento, largura, altura, tubo) '+
+        'VALUES ('+pedido+', '+cliente+', '+modelo+', '+colecao+', '+cor+', '+acionamento+', '+largura+', '+altura+', '+tubo+')')
+
     def loginVerification(self, name, password):
         self.cursor.execute('SELECT userName, password FROM login')
         for user in self.cursor.fetchall():
